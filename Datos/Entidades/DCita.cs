bd15c7056d7c29f66fb218bcd037363f858d106d
Alexander Galan <Alexander Galan@DESKTOP-Q6JBD0N>
2024-04-10 19:46:34 -0600

    Crecion de las tablas con sus migraciones

---
+++ Datos/Entidades/DCita.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Entidades
+{
+    public class Cita
+    {
+        public int CitaId { get; set; }
+        public int MedicoId { get; set; }
+        public int PacienteId { get; set; }
+        public DateTime FechaCita { get; set; }
+        public bool Estado { get; set; }
+
+        public virtual Medico Medico { get; set; }
+        public virtual Paciente Paciente { get; set; }
+    }
+}
