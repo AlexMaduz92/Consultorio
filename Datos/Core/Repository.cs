0bc74c279f39c84cd3cc326a564192147d776d6b
Alexander Galan <Alexander Galan@DESKTOP-Q6JBD0N>
2024-04-10 21:33:00 -0600

    Implementacion de Core, Repositorio y unitofwork

---
+++ Datos/Core/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Core
+{
+    public abstract class Repository<T> where T : class
+    {
+        protected readonly DbContext Context;
+        protected readonly DbSet<T> DbSet;
+
+        protected Repository(DbContext context)
+        {
+            Context = context;
+            DbSet = context.Set<T>();
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return DbSet;
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            DbSet.Attach(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+    }
+}
